c4a80a5f8d8fb8823d2a53528e2a83e7bcc14643
king-sovan <156076876+king-sovan@users.noreply.github.com>
2024-07-15 19:54:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+
+	const firstSpace = s.indexOf(' ');
+
+	if (firstSpace == -1) {
+		return s;
+	}
+
+	return s.subString(0, firstSpace);
 }
 
 // Do not change the code below
